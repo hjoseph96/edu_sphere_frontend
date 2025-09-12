4007af5c765a6a91737fa50086cdfd919f93c340
Herbert Joseph <techniquea2z@gmail.com>
2025-09-11 22:36:11 -0400

    fix(prod): use heroku api link

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with default config
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/v1', // https://edu-sphere-api-f52fa37eb762.herokuapp.com
+  baseURL: "https://edu-sphere-api-f52fa37eb762.herokuapp.com/api/v1", // https://edu-sphere-api-f52fa37eb762.herokuapp.com
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
